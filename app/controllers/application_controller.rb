aec2aeec2d6d97cfff59338580dab0160f54c7a6
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-09 15:32:24 -0500

    Added before_action authorization method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
+
+  before_action :authorized
+
+  def authorized
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+  
 end
