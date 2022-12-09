2999928887eade9c827ef1c7718835d5c81b33c7
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-09 15:32:39 -0500

    Added create and show methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  # POST /users
+  def create
+    @user = User.create!(user_params)
+    session[:user_id] = @user.id
+    render json: @user, status: :created
+  end
+  
+  # GET /me
+  def show
+    @user = User.find(session[:user_id])
+    render json: @user, includes: :user_habits
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+
 end
