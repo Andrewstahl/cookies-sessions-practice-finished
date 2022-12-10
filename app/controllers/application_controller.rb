5985b838aeb88b131abeaaa88714f152b299d322
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-10 15:22:36 -0500

    Changed to API application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   # Added this to prevent default Cross-Site REquest Forgery (CSRF)
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # about that here - 
   # https://guides.rubyonrails.org/security.html#cross-site-request-forgery-csrf
   # UNNECESSARY IF YOU DO rails new appname --api WHEN CREATING YOUR APP
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
 
   before_action :authorized
 
