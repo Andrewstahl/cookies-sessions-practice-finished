a765caae28f8a21ba1bbda543349f0e6b5b532c2
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-10 14:40:34 -0500

    Removed default security for CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
 
+  # Added this to prevent default Cross-Site REquest Forgery (CSRF)
+  # security measures from blocking my API request. Read more 
+  # about that here - 
+  # https://guides.rubyonrails.org/security.html#cross-site-request-forgery-csrf
+  # UNNECESSARY IF YOU DO rails new appname --api WHEN CREATING YOUR APP
+  protect_from_forgery with: :null_session
+
   before_action :authorized
 
   def authorized
     return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
   end
-  
+
 end
